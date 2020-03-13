009d731d42d4cd34049ccd18e31231ebbfaf8f7d
Tobias Wittwer <t.wittwer95@gmail.com>
2020-03-13 09:34:51 +0100

    Add moduleNameMapper for `base.element.css`

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
     '^.+\\.(ts|html)$': 'ts-jest'
   },
   transformIgnorePatterns: ['node_modules/(?!@clr/core|lit-element|lit-html|ramda)'],
+  moduleNameMapper: {
+    'base.element.css': '@clr/core/common/base/base.element.css.js',
+  },
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   collectCoverage: true,
